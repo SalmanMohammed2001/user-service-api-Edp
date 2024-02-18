03484dd384ce57da4151575d80583429eb4af0d7
SalmanMohammed2001 <salmannisthar123@gmail.com>
2024-02-19 01:31:58 +0530

    create ShippingRepo  and BellingRepo entity

---
+++ src/main/java/com/eadp/userserviceapi/repo/BellingRepo.java
@@ -0,0 +1,8 @@
+package com.eadp.userserviceapi.repo;
+
+import com.eadp.userserviceapi.entity.ShippingAddress;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BellingRepo extends JpaRepository<BellingRepo,Long> {
+
+}
---
+++ src/main/java/com/eadp/userserviceapi/repo/ShippingRepo.java
@@ -0,0 +1,14 @@
+package com.eadp.userserviceapi.repo;
+
+import com.eadp.userserviceapi.entity.ShippingAddress;
+import com.eadp.userserviceapi.entity.User;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface ShippingRepo extends JpaRepository<ShippingAddress,Long> {
+
+}
